8fd67ca71cc04d9ff3712d3ac89747d520b789aa
szg268 <zgx168268@sina.com>
2024-08-14 11:33:51 +0800

    creat

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "src/cubic.cpp"
+#include <SDL2/SDL.h>
+SDL_Window *window = NULL;
+SDL_Surface *surface = NULL;
+ 
+//main函数中的参数不能省略，不然会报错
+int main(int args, char *argv[])
+{
+   
+    
+ 
+    return 0;
+}
